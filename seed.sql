4f3fb05f9c56e1448586f2b67c9e8879439b5fb2
William Grundler <wrgrundler@gmail.com>
2020-05-28 17:44:04 -0700

    seed department

---
+++ seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO departments (name)
+VALUES ("Engineering");
+
+INSERT INTO departments (name)
+VALUES ("Sales");
+
+INSERT INTO departments (name)
+VALUES ("Finance");
+
+INSERT INTO departments (name)
+VALUES ("Legal");
