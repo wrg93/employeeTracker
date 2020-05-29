2f5fee58958119e7f8d9309ba3f94f33fd1804af
William Grundler <wrgrundler@gmail.com>
2020-05-28 17:29:51 -0700

    boiler plate js

---
+++ employeeTracker.js
@@ -0,0 +1,28 @@
+// Dependencies
+var express = require("express");
+var mysql = require("mysql");
+
+// Create express app instance.
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// MySQL DB Connection Information
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Chandler!93",
+  database: "employeeTracker_DB"
+});
+
+// Initiate MySQL Connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
